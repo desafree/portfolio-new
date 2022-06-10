e798ffedd353059e2ec07d7a851a36e496e4fc62
desafree <nicoladesanctis99@gmail.com>
2022-06-10 16:11:17 +0200

    fix easing

--- script.js
+++ script.js
@@ -1,56 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// const myTags = [
-//   'JavaScript',
-//   'CSS',
-//   'HTML',
-//   'C',
-//   'C++',
-//   'React',
-//   'Python',
-//   'Java',
-//   'git',
-//   'django',
-//   'Node.js',
-//   'OpenCV',
-//   'GCP',
-//   'MySQL',
-//   'jQuery',
-//   'JavaScript',
-//   'CSS',
-//   'HTML',
-//   'C',
-//   'C++',
-//   'React',
-//   'Python',
-//   'Java',
-//   'git',
-//   'django',
-//   'Node.js',
-//   'OpenCV',
-//   'GCP',
-//   'MySQL',
-//   'jQuery',
-// ];
-
-// var tagCloud = TagCloud('.content', myTags, {
-//   // radius in px
-//   radius: 300,
-
-//   // animation speed
-//   // slow, normal, fast
-//   maxSpeed: 'fast',
-//   initSpeed: 'normal',
-
-//   // 0 = top
-//   // 90 = left
-//   // 135 = right-bottom
-//   direction: 135,
-
-//   // interact with cursor move on mouse out
-//   keep: true,
-// });
-
+// header animation
 let header = gsap
   .timeline()
   .from('ul li', { y: -50, opacity: 0, stagger: 0.05 })
@@ -58,20 +8,22 @@ let header = gsap
   .from('header h1', { x: -100, opacity: 0 }, '-=.5')
   .from('.contact-me', { xPercent: 50, opacity: 0 }, '<');
 
+// about animation
 let about = gsap
   .timeline({
     scrollTrigger: {
       trigger: '.about',
       start: 'top 50%',
-      //   start:"top 50%"
     },
   })
-  .from('.about .text', { x: 50, opacity: 0 })
+  .from('.about .text', { x: 50, opacity: 0, ease: Power0.easeOut })
   .from(
     '.about .img-container',
-    { scale: 0.8, opacity: 0, ease: 'linear' },
+    { scale: 0.8, opacity: 0, ease: Power0.easeOut, duration: 0.3 },
     '<.2'
   );
+
+// projects animation
 
 let projects = gsap
   .timeline({
@@ -89,50 +41,11 @@ let projects = gsap
     '-=.5'
   );
 
-let workIntro = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: '.work .intro',
-      start: 'top 70%',
-      //   start:"top 50%"
-    },
-  })
-  .from('.work .intro h3', { x: -20, opacity: 0, stagger: 0.1 })
-  .from('.work .intro p', { x: 20, opacity: 0, stagger: 0.1 }, '<');
-
-let workText = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: '.work .text',
-      start: 'top 70%',
-      //   start:"top 50%"
-    },
-  })
-  .from('.work .text h4', { x: 20, opacity: 0, stagger: 0.1 })
-  .from('.work .text p', { x: -20, opacity: 0, stagger: 0.1 }, '<');
-
-let contact = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: 'footer',
-      start: 'top 70%',
-      //   start:"top 50%"
-    },
-  })
-  .from('footer h3', { x: -20, opacity: 0 })
-  .from('footer .contact-box', { scale: 0.8, opacity: 0 }, '<');
-
 const items = document.querySelectorAll('.item');
-
-// items[1].addEventListener('click', () => {
-//   gsap.to('.text-back', { y: '0' });
-// });
-// gsap.from('.text-back', { y: '100%' });
-
 items.forEach((item) => {
   let tl = gsap
-    .timeline({ paused: true, default: { ease: 'linear' } })
-    .to(item.querySelector('.text-back'), { y: '0' })
+    .timeline({ paused: true })
+    .to(item.querySelector('.text-back'), { y: '0', ease: Power0.easeOut })
     .from(
       item.querySelectorAll('.text-back > *'),
       {
@@ -154,11 +67,54 @@ items.forEach((item) => {
   });
 });
 
+// work animations
+let workIntro = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.work .intro',
+      start: 'top 70%',
+    },
+  })
+  .from('.work .intro h3', { x: -20, opacity: 0, stagger: 0.1 })
+  .from('.work .intro p', { x: 20, opacity: 0, stagger: 0.1 }, '<');
+
+let workText = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.work .text',
+      start: 'top 70%',
+      //   start:"top 50%"
+    },
+  })
+  .from('.work .text h4', { x: 20, opacity: 0, stagger: 0.1 })
+  .from('.work .text p', { x: -20, opacity: 0, stagger: 0.1 }, '<');
+
+// contact animation
+let contact = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: 'footer',
+      start: 'top 70%',
+      //   start:"top 50%"
+    },
+  })
+  .from('footer h3', { x: -20, opacity: 0 })
+  .from('footer .contact-box', { scale: 0.8, opacity: 0 }, '<');
+
+// navigaiton animation
 const tlNavMobile = gsap
   .timeline({ paused: true })
-  .to('.burger', { opacity: 0, scale: 0.5 })
-  .to('nav .mobile .nav-container', { visibility: 'visible' }, '<.2')
-  .from('nav .mobile .nav-container', { x: '100%' }, '<');
+  .to('.burger', { scale: 0.5, opacity: 0.2 })
+  .to(
+    'nav .mobile .nav-container',
+    { visibility: 'visible', duration: 0.2 },
+    '<.2'
+  )
+  .from(
+    'nav .mobile .nav-container',
+    { x: '100%', ease: Power0.easeOut, duration: 0.3 },
+    '<'
+  );
 
 const menuIcon = document.querySelector('.burger');
 menuIcon.addEventListener('click', () => {
